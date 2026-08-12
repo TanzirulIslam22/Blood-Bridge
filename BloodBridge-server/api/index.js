ba48e03c04a9f751b28a8520bff0635d9e79dc17
Tanzirul Islam <tanzirul.islam56@gmail.com>
2026-08-12 10:10:35 +0600

    fix: connect MongoDB in Vercel serverless entry (api/index.js) so DB routes work on deployment

--- BloodBridge-server/api/index.js
+++ BloodBridge-server/api/index.js
@@ -1,2 +1,10 @@
+const mongoose = require('mongoose');
+
+if (process.env.VERCEL && process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI).catch((err) => {
+    console.error('MongoDB connection error:', err.message);
+  });
+}
+
 const app = require('../server');
 module.exports = app;
